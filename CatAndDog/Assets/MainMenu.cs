b6058d843f2cfa7c9304149902db8bafe23e94be
XxEmilxX <emiljoesfrom@gmail.com>
2023-11-23 13:08:08 +0100

    Updated Main Menu v2.0

--- CatAndDog/Assets/MainMenu.cs
+++ CatAndDog/Assets/MainMenu.cs
@@ -1,10 +1,21 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI volumeText;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Toggle fullscreenToggle;
+
+    // Initializing Options
+    private void Start()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1);
+        fullscreenToggle.isOn = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
+    }
+
 
     // Menu Functions:
 
@@ -25,9 +36,12 @@ public class MainMenu : MonoBehaviour
     public void SetVolume(float volume)
     {
         AudioListener.volume = volume;
+        volumeText.text = "Volume: " + (volume * 100).ToString("0") + "%";
+        PlayerPrefs.SetFloat("Volume", volume);
     }
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
     }
 }
