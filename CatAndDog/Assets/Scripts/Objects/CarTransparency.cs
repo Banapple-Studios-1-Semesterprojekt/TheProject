9ae3e65a07e676b41576f82123ea72eb7ceb6f4b
CupcakeFlip <tiffanylea8@gmail.com>
2023-11-30 13:37:48 +0100

    Car done

--- CatAndDog/Assets/Scripts/Objects/CarTransparency.cs
+++ CatAndDog/Assets/Scripts/Objects/CarTransparency.cs
@@ -1,29 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class CarTransparency : MonoBehaviour
-{
+public class CarTransparency : MonoBehaviour {
     public GameObject car; // Reference to the car object.
     public SpriteRenderer frontSpriteRenderer; // Reference to the front of the car's sprite renderer.
     public bool isCharacterInside = false; // Flag to track if the character is inside the car.
+    public bool catInCar = false;
+    public bool dogInCar = false;
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Cat") || other.CompareTag("Dog"))
-        {
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Cat") || other.CompareTag("Dog")) {
+            if (other.CompareTag("Cat")) {
+                catInCar = true;
+            }
+            if (other.CompareTag("Dog")) {
+                dogInCar = true;
+            }
             isCharacterInside = true;
             // Adjust the transparency of the front of the car.
             Color frontColor = frontSpriteRenderer.color;
-            frontColor.a = 0.5f; // Set transparency to 50%.
+            frontColor.a = 0.01f; // Set transparency to 1%.
             frontSpriteRenderer.color = frontColor;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Cat") || other.CompareTag("Dog"))
-        {
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Cat")) {
+            catInCar = false;
+        }
+        if (other.CompareTag("Dog")) {
+            dogInCar = false;
+        }
+        if (!catInCar && !dogInCar) {
             isCharacterInside = false;
             // Restore the transparency of the car's front.
             Color frontColor = frontSpriteRenderer.color;
--- CatAndDog/Assets/Scripts/Objects/CarUnlockDoor.cs
+++ CatAndDog/Assets/Scripts/Objects/CarUnlockDoor.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarUnlockDoor : MonoBehaviour
-{
+public class CarUnlockDoor : MonoBehaviour {
     public BoxCollider2D door;
-    public SpriteRenderer leftDoorSpriteRenderer;
+    public SpriteRenderer bothDoorsOpen;
+    public SpriteRenderer rightDoor;
+    //public GameObject car;
+    //   public SpriteRenderer leftDoorSpriteRenderer;
 
     public bool doorLocked = true;
 
-    public void setDoorOpen()
-    {
-        door.enabled = false;
-        if (doorLocked)
-        {
+    public void setDoorOpen() {
+        if (doorLocked) {
             // Unlock the left door when the cat approaches it from the right side inside the car.
             doorLocked = false;
-            leftDoorSpriteRenderer.color = Color.green; // Set to green (unlocked) color.
+            Destroy(door);
+            bothDoorsOpen.enabled = true;
+            rightDoor.enabled = false;
         }
     }
 }
